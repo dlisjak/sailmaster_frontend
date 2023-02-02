48ed15896ab9c33c8d8f6fcc7d6b4c08735ad896
Domen <domen.lisjak@gmail.com>
2023-02-02 12:46:10 +0100

    add lazy eager loading to gallery

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -12,19 +12,24 @@ export default function Gallery({ yacht }) {
           <YouTube videoId={youtube} opts={{ height: '210', width: '100%' }} />
         </Carousel.Item>
       )}
-      {pictures.map((image, i) => (
-        <Carousel.Item key={`${image}-${i}`}>
-          <div className="featuredYacht__imageContainer relative w-full h-full flex overflow-hidden mb-1 aspect-[16/9] md:aspect-[4/3]">
-            <Image
-              className="carousel-offer-item object-cover"
-              src={image}
-              alt={`${yacht.yacht_model.name} ${yacht.yacht_model.category_name}`}
-              width={480}
-              height={325}
-            />
-          </div>
-        </Carousel.Item>
-      ))}
+      {pictures.map((image, i) => {
+        const loading = i === 0 ? 'eager' : 'lazy';
+
+        return (
+          <Carousel.Item key={`${image}-${i}`}>
+            <div className="featuredYacht__imageContainer relative mb-1 flex aspect-[16/9] h-full w-full overflow-hidden md:aspect-[4/3]">
+              <Image
+                className="carousel-offer-item object-cover"
+                src={image}
+                alt={`${yacht.yacht_model.name} ${yacht.yacht_model.category_name}`}
+                width={480}
+                height={325}
+                loading={loading}
+              />
+            </div>
+          </Carousel.Item>
+        );
+      })}
     </Carousel>
   );
 }
