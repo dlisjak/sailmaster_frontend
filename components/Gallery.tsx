713930809fb29542de4c8a2b4ec500e117ec9d44
Domen <domen.lisjak@gmail.com>
2023-03-09 15:09:47 +0100

    add gallery changes

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -2,16 +2,20 @@ import Slider from 'react-slick';
 
 import Carousel from 'react-bootstrap/Carousel';
 import YouTube from 'react-youtube';
+import { useState } from 'react';
 
 export default function Gallery({ images, youtube }) {
+  const [slideIndex, setSlideIndex] = useState(0);
+
   const settings = {
     dots: true,
     infinite: true,
+    lazyLoad: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     appendDots: (dots) => <ol className="carousel-indicators">{dots}</ol>,
-    customPaging: () => <div className="" />,
+    customPaging: (i) => <button className="w-auto" />,
   };
 
   return (
@@ -24,10 +28,7 @@ export default function Gallery({ images, youtube }) {
       {images.map((image, i) => {
         return (
           <Carousel.Item key={`${image}-${i}`}>
-            <div
-              className="carousel-offer-item"
-              style={{ backgroundImage: `url("${image.replaceAll(' ', '%20')}")` }}
-            />
+            <div className="carousel-offer-item" style={{ backgroundImage: `url("${image}")` }} />
           </Carousel.Item>
         );
       })}
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import dynamic from "next/dynamic.js";
 import { appWithTranslation } from 'next-i18next'
 import nextI18NextConfig from '../next-i18next.config.js'
 import SSRProvider from 'react-bootstrap/SSRProvider';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 
 import Navigation from "../components/Navigation";
 import Footer from "../components/common/Footer";
@@ -11,7 +13,6 @@ const InquiryModal = dynamic(() => import("../components/Inquiry"));
 const PromoModal = dynamic(() => import("../components/PromoModal"));
 const CookieLaw = dynamic(() => import("../components/common/CookieLaw"));
 const ScrollToTop = dynamic(() => import("react-scroll-to-top"));
-// import ScrollToTop from "react-scroll-to-top";
 
 import ErrorBoundary from "../components/ErrorBoundary";
 // import { language } from '../constants'
@@ -19,19 +20,34 @@ import ErrorBoundary from "../components/ErrorBoundary";
 import '../styles/main.css';
 import "../node_modules/flag-icons/css/flag-icons.min.css";
 
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn:
+      "https://dd8fab38768a411d988af3a213bd039c@o38387.ingest.sentry.io/5559422",
+    autoSessionTracking: true,
+    integrations: [new Integrations.BrowserTracing()],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
+}
+
+
 const MyApp = ({ Component, pageProps, router }) => {
   const route = router.route;
+  const path = router.asPath;
   const lang = process.env.NEXT_PUBLIC_REACT_APP_LANGUAGE;
   const [showInquiry, setShowInquiry] = useState(false);
   const [showModal, setShowModal] = useState(false);
 
   // useEffect(() => {
   //   setTimeout(() => {
-  //     window.ga.getAll()[0].set("page", location.pathname + location.search);
+  //     window.ga.getAll()[0].set("page", path);
   //     window.ga.getAll()[0].send("pageview");
   //   }, 0)
   //   return
-  // }, [route])
+  // }, [path])
 
   useEffect(() => {
     const handlePromoModal = () => {
@@ -51,7 +67,9 @@ const MyApp = ({ Component, pageProps, router }) => {
     <SSRProvider>
       <ScrollToTop smooth color="#ceb896" />
       <Navigation setShowInquiry={setShowInquiry} />
-      <Component {...pageProps} route={route} key={route} lang={lang} />
+      <ErrorBoundary>
+        <Component {...pageProps} route={route} key={route} lang={lang} />
+      </ErrorBoundary>
       <Footer lang={lang} />
       <CookieLaw />
       {showModal && <PromoModal onHide={() => setShowModal(false)} />}
