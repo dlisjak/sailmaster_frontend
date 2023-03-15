fd1dda41fb1a33dda9986f8fd38bc42978011005
Domen <domen.lisjak@gmail.com>
2023-03-15 06:04:18 +0100

    add rewrites

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ module.exports = withBundleAnalyzer({
         destination: "/:path*"
       },
       {
+        source: "/admin/",
+        destination: "https://api.thesailmaster.si/admin/"
+      },
+      {
         source: "/:path*",
         destination: "https://api.thesailmaster.si/:path*"
       },
