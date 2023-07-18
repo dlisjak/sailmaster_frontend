e0c04b45abe81145414fa95bb201dd392d5c55e1
Domen <domen.lisjak@gmail.com>
2023-07-18 21:38:52 +0200

    optimise images

--- pages/_app.js
+++ pages/_app.js
@@ -1,57 +1,55 @@
 import { useEffect, useState } from "react";
-import dynamic from "next/dynamic.js";
 import { appWithTranslation } from 'next-i18next'
 import nextI18NextConfig from '../next-i18next.config.js'
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
+// import * as Sentry from "@sentry/react";
+// import { Integrations } from "@sentry/tracing";
 import Script from 'next/script';
 
 import Navigation from "../components/Navigation";
 import Footer from "../components/common/Footer";
 
-const InquiryModal = dynamic(() => import("../components/Inquiry"));
-const PromoModal = dynamic(() => import("../components/PromoModal"));
-const CookieLaw = dynamic(() => import("../components/common/CookieLaw"));
-const ScrollToTop = dynamic(() => import("react-scroll-to-top"));
+import InquiryModal from "../components/Inquiry";
+import PromoModal from "../components/PromoModal";
+import CookieLaw from "../components/common/CookieLaw";
+import ScrollToTop from "react-scroll-to-top";
 
 import ErrorBoundary from "../components/ErrorBoundary";
-// import { language } from '../constants'
 
 import '../styles/main.css';
 import "../node_modules/flag-icons/css/flag-icons.min.css";
 
-if (process.env.NODE_ENV === "production") {
-  Sentry.init({
-    dsn:
-      "https://dd8fab38768a411d988af3a213bd039c@o38387.ingest.sentry.io/5559422",
-    autoSessionTracking: true,
-    integrations: [new Integrations.BrowserTracing()],
+// if (process.env.NODE_ENV === "production") {
+//   Sentry.init({
+//     dsn:
+//       "https://dd8fab38768a411d988af3a213bd039c@o38387.ingest.sentry.io/5559422",
+//     autoSessionTracking: true,
+//     integrations: [new Integrations.BrowserTracing()],
 
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
-    tracesSampleRate: 1.0,
-  });
-}
+//     // We recommend adjusting this value in production, or using tracesSampler
+//     // for finer control
+//     tracesSampleRate: 1.0,
+//   });
+// }
 
 
 const MyApp = ({ Component, pageProps, router }) => {
   const route = router.route;
   const lang = process.env.NEXT_PUBLIC_REACT_APP_LANGUAGE;
   const [showInquiry, setShowInquiry] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+  // const [showModal, setShowModal] = useState(false);
 
-  useEffect(() => {
-    const handlePromoModal = () => {
-      if (!localStorage.getItem("promoModal")) {
-        setTimeout(() => {
-          setShowModal(true)
-          localStorage.setItem("promoModal", 1)
-        }, 3000)
-      }
-    }
+  // useEffect(() => {
+  //   const handlePromoModal = () => {
+  //     if (!localStorage.getItem("promoModal")) {
+  //       setTimeout(() => {
+  //         setShowModal(true)
+  //         localStorage.setItem("promoModal", 1)
+  //       }, 3000)
+  //     }
+  //   }
 
-    handlePromoModal();
-  }, []);
+  //   handlePromoModal();
+  // }, []);
 
 
   return (
@@ -76,7 +74,7 @@ const MyApp = ({ Component, pageProps, router }) => {
       </ErrorBoundary>
       <Footer lang={lang} />
       <CookieLaw />
-      {showModal && <PromoModal onHide={() => setShowModal(false)} />}
+      {/* {showModal && <PromoModal onHide={() => setShowModal(false)} />} */}
       {showInquiry && <InquiryModal onClose={() => setShowInquiry(false)} />}
     </>
   );
